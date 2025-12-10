c8c9e6dfd65742b77c9963039784133c487ea946
Keshore333 <keshore.ecom@gmail.com>
2025-12-10 11:47:39 +0530

    Change HOURS_LOCK from 24 to 1

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ const puzzleConfig = {
 
 /* ---------- PROGRESS + 12-HOUR UNLOCK LOGIC ---------- */
 
-const HOURS_LOCK = 24;
+const HOURS_LOCK = 1;
 const MS_PER_HOUR = 1000 * 60 * 60;
 
 let solvedDays = JSON.parse(localStorage.getItem("solvedDays") || "{}");
